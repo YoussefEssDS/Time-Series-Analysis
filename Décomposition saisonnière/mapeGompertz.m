1f94ca69def4282e8691f9feae0fb326a2128f20
YoussefEssDS <oyoussefesseddiq@gmail.com>
2019-03-19 16:49:49 +0100

    models by matlab

---
+++ Décomposition saisonnière/mapeGompertz.m
@@ -0,0 +1,14 @@
+% On vas creer une fonction qui retourne la mape du modele exponentiel
+% modifie comptabilisé à partir des données PIB Belgique & RFA.
+% On a besoin de donner les valeurs de alpha, beta et gamma calculées à
+% l'aide de la methode des 3 pts sur Excel. 
+% x=[-2.261454542,0.909464861,5.47328536]
+function mape=mapeGompertz(x)
+    read_variables; % lecture des variables à partir du fichier Excel.
+    mape=0;
+    len=length(y);
+    for i=1:len
+        mape=mape+100*abs(exp(x(1)*x(2)^i+x(3)-y(i)))/y(i);
+    end
+    mape=mape/len;
+end
--- Décomposition saisonnière/mapeLogistic.m
+++ Décomposition saisonnière/mapeLogistic.m
@@ -2,13 +2,13 @@
 % modifie comptabilisé à partir des données PIB Belgique & RFA.
 % On a besoin de donner les valeurs de alpha, beta et gamma calculées à
 % l'aide de la methode des 3 pts sur Excel. 
-% x=[177.3132181,1.036758222,-153.8882353]
-function mape=mapeExpMod(x)
+% x=[0.032028309,0.796217279,0.006943227]
+function mape=mapeLogistic(x)
     read_variables; % lecture des variables à partir du fichier Excel.
     mape=0;
     len=length(y);
     for i=1:len
-        mape=mape+100*abs(x*beta^i+gamma-y(i))/y(i);
+        mape=mape+100*abs(1/(x(1)*x(2)^i+x(3)-y(i)))/y(i);
     end
     mape=mape/len;
 end
