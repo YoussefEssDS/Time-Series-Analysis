6ae20928ea5d5c79e3f2ebb2f86a5f71e52a31d2
YoussefEssDS <oyoussefesseddiq@gmail.com>
2019-04-06 14:45:25 +0100

    winters-holt

---
+++ Lissage Holt-Winters/Additif/optimalParHW.m
@@ -0,0 +1,10 @@
+% We will use the constrained minimization function of Matlab to find the
+% best parameters that yield the minimal MAPE. 
+
+function [mapeMin,parameters]=optimalParHW(fun,x0)
+    lecture;
+    A=[];b=[];
+    Aeq=[];Beq=[];
+    lb=[-Inf,-Inf,0,0,0];ub=[Inf,Inf,1,1,1];
+    [mapeMin,parameters]=fmincon(fun,x0,A,b,Aeq,Beq,lb,ub);
+end
