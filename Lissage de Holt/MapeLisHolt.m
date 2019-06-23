798547591b1d71c0a453d12f337c82669229e268
YoussefEssDS <oyoussefesseddiq@gmail.com>
2019-06-23 19:05:55 +0100

    Generalized smoothing

--- Lissage de Holt/MapeLisHolt.m
+++ Lissage de Holt/MapeLisHolt.m
@@ -1,21 +1,17 @@
-% Like we did with Excel this function calculates the mape for the time
-% serie try it with: x=[mean([y(1),y(2),y(3)]),(y(3)-y(1))/2,0.4,0.3] and
-% check if it would give same result as in the Excel file. (hint: It
-% would!)
-function mape=MapeLisHolt(x)
+function mse=MseLisHolt(x0)
     lecture;
-    mape=0;
-    St=x(1);
-    Tt=x(2);
-    lambda=x(3);
-    mu=x(4);
-    len=length(y);
+    mse=0;
+    St=x0(1);
+    Tt=x0(2);
+    lambda=x0(3);
+    mu=x0(4);
+    len=length(x);
     yt=St+Tt;
     for i=3:len
-        mape=mape+100*abs(yt-y(i))/y(i);
+        mse=mse+(yt-x(i))^2;
         tempT=Tt;
         tempS=St;
-        St=lambda*y(i)+(1-lambda)*(tempS-tempT);
+        St=lambda*x(i)+(1-lambda)*(tempS-tempT);
         Tt=mu*(St-tempS)+(1-mu)*tempT;
         yt=St+Tt;
     end
